c01db2c236669f7ef3579d70d26d44d82c17ba37
Blicey <47247595+Blicey@users.noreply.github.com>
2021-08-05 22:38:45 +0800

    Add files via upload

---
+++ Core/Src/main.c
@@ -0,0 +1,505 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+SPI_HandleTypeDef hspi2;
+
+UART_HandleTypeDef huart1;
+
+osThreadId defaultTaskHandle;
+osThreadId CC1101_MHandle;
+osThreadId CC1101_SHandle;
+/* USER CODE BEGIN PV */
+CC1101_HandleTypeDef CC1101_M;
+CC1101_HandleTypeDef CC1101_S;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+
+static void MX_GPIO_Init(void);
+
+static void MX_SPI1_Init(void);
+
+static void MX_SPI2_Init(void);
+
+static void MX_USART1_UART_Init(void);
+
+void StartDefaultTask(void const *argument);
+
+void StartTask_CC1101_M(void const *argument);
+
+void StartTask_CC1101_S(void const *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void) {
+    /* USER CODE BEGIN 1 */
+
+    /* USER CODE END 1 */
+
+    /* MCU Configuration--------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+    RetargetInit(&huart1);
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_SPI1_Init();
+    MX_SPI2_Init();
+    MX_USART1_UART_Init();
+    /* USER CODE BEGIN 2 */
+    printf("Hello World.\n");
+    /* USER CODE END 2 */
+
+    /* USER CODE BEGIN RTOS_MUTEX */
+    /* add mutexes, ... */
+    /* USER CODE END RTOS_MUTEX */
+
+    /* USER CODE BEGIN RTOS_SEMAPHORES */
+    /* add semaphores, ... */
+    /* USER CODE END RTOS_SEMAPHORES */
+
+    /* USER CODE BEGIN RTOS_TIMERS */
+    /* start timers, add new ones, ... */
+    /* USER CODE END RTOS_TIMERS */
+
+    /* USER CODE BEGIN RTOS_QUEUES */
+    /* add queues, ... */
+    /* USER CODE END RTOS_QUEUES */
+
+    /* Create the thread(s) */
+    /* definition and creation of defaultTask */
+    osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+    defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+    /* definition and creation of CC1101_M */
+    osThreadDef(CC1101_M, StartTask_CC1101_M, osPriorityLow, 0, 128);
+    CC1101_MHandle = osThreadCreate(osThread(CC1101_M), NULL);
+
+    /* definition and creation of CC1101_S */
+    osThreadDef(CC1101_S, StartTask_CC1101_S, osPriorityLow, 0, 128);
+    CC1101_SHandle = osThreadCreate(osThread(CC1101_S), NULL);
+
+    /* USER CODE BEGIN RTOS_THREADS */
+    /* add threads, ... */
+    /* USER CODE END RTOS_THREADS */
+
+    /* Start scheduler */
+    osKernelStart();
+
+    /* We should never get here as control is now taken by the scheduler */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while(1) {
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+    }
+    /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Configure the main internal regulator output voltage
+    */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void) {
+
+    /* USER CODE BEGIN SPI1_Init 0 */
+
+    /* USER CODE END SPI1_Init 0 */
+
+    /* USER CODE BEGIN SPI1_Init 1 */
+
+    /* USER CODE END SPI1_Init 1 */
+    /* SPI1 parameter configuration*/
+    hspi1.Instance = SPI1;
+    hspi1.Init.Mode = SPI_MODE_MASTER;
+    hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+    hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+    hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+    hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+    hspi1.Init.NSS = SPI_NSS_SOFT;
+    hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+    hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+    hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    hspi1.Init.CRCPolynomial = 10;
+    if(HAL_SPI_Init(&hspi1) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN SPI1_Init 2 */
+
+    /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void) {
+
+    /* USER CODE BEGIN SPI2_Init 0 */
+
+    /* USER CODE END SPI2_Init 0 */
+
+    /* USER CODE BEGIN SPI2_Init 1 */
+
+    /* USER CODE END SPI2_Init 1 */
+    /* SPI2 parameter configuration*/
+    hspi2.Instance = SPI2;
+    hspi2.Init.Mode = SPI_MODE_MASTER;
+    hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+    hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+    hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+    hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+    hspi2.Init.NSS = SPI_NSS_SOFT;
+    hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+    hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+    hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    hspi2.Init.CRCPolynomial = 10;
+    if(HAL_SPI_Init(&hspi2) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN SPI2_Init 2 */
+
+    /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void) {
+
+    /* USER CODE BEGIN USART1_Init 0 */
+
+    /* USER CODE END USART1_Init 0 */
+
+    /* USER CODE BEGIN USART1_Init 1 */
+
+    /* USER CODE END USART1_Init 1 */
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200;
+    huart1.Init.WordLength = UART_WORDLENGTH_9B;
+    huart1.Init.StopBits = UART_STOPBITS_2;
+    huart1.Init.Parity = UART_PARITY_EVEN;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    if(HAL_UART_Init(&huart1) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN USART1_Init 2 */
+
+    /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : PA4 */
+    GPIO_InitStruct.Pin = GPIO_PIN_4;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : PC4 */
+    GPIO_InitStruct.Pin = GPIO_PIN_4;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : PC5 */
+    GPIO_InitStruct.Pin = GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : PB1 */
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : PB12 */
+    GPIO_InitStruct.Pin = GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const *argument) {
+    /* USER CODE BEGIN 5 */
+    /* Infinite loop */
+    for(;;) {
+        osDelay(1000);
+    }
+    /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartTask_CC1101_M */
+/**
+* @brief Function implementing the CC1101_M thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask_CC1101_M */
+void StartTask_CC1101_M(void const *argument) {
+    /* USER CODE BEGIN StartTask_CC1101_M */
+    uint8_t dummy_cmd[PKT_SIZE] = { 0 };
+    uint8_t frame_t[PKT_SIZE] = { 0 };
+    uint8_t frame_r[PKT_SIZE] = { 0 };
+    CC1101_M.spi_port = &hspi1;
+    CC1101_M.CSN_port = GPIOC;
+    CC1101_M.CSN_pin = GPIO_PIN_5;
+    CC1101_M.GDO0_port = GPIOA;
+    CC1101_M.GDO0_pin = GPIO_PIN_4;
+    CC1101_Init(&CC1101_M);
+    CC1101_Set_Mode(&CC1101_M, TX_MODE);
+    printf("Master init finished.\n");
+    Frame_Write(dummy_cmd, &CC1101_M);
+    CC1101_Send(&CC1101_M);
+    int rx_timeout_cnt = 0;
+#define RX_TIMEOUT_LIMIT    10
+    /* Infinite loop */
+    for(;;) {
+        while(!CC1101_Fetch(&CC1101_M)) {
+            rx_timeout_cnt++;
+            osDelay(250);
+            if(rx_timeout_cnt >= RX_TIMEOUT_LIMIT) {
+                Frame_Write(dummy_cmd, &CC1101_M);
+                CC1101_Send(&CC1101_M);
+                rx_timeout_cnt = 0;
+            }
+        }
+        Frame_Read(frame_r, &CC1101_M);
+        printf("Master new pkg received.\n");
+        osDelay(250);
+        Frame_Write(frame_t, &CC1101_M);
+        CC1101_Send(&CC1101_M);
+        osDelay(250);
+    }
+    /* USER CODE END StartTask_CC1101_M */
+}
+
+/* USER CODE BEGIN Header_StartTask_CC1101_S */
+/**
+* @brief Function implementing the CC1101_S thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask_CC1101_S */
+void StartTask_CC1101_S(void const *argument) {
+    /* USER CODE BEGIN StartTask_CC1101_S */
+    uint8_t frame_t[PKT_SIZE] = { 0 };
+    uint8_t frame_r[PKT_SIZE] = { 0 };
+    CC1101_S.spi_port = &hspi2;
+    CC1101_S.CSN_port = GPIOB;
+    CC1101_S.CSN_pin = GPIO_PIN_12;
+    CC1101_S.GDO0_port = GPIOB;
+    CC1101_S.GDO0_pin = GPIO_PIN_1;
+    CC1101_Init(&CC1101_S);
+    CC1101_Set_Mode(&CC1101_S, RX_MODE);
+    //printf("Slave init finished.\n");
+    /* Infinite loop */
+    for(;;) {
+        while(!CC1101_Fetch(&CC1101_S)) {
+            uint8_t status = CC1101_Read_Status(&CC1101_S, CC1101_MARCSTATE);
+            osDelay(250);
+        }
+        Frame_Read(frame_r, &CC1101_S);
+        //printf("Slave new pkg received.\n");
+        Frame_Write(frame_t, &CC1101_S);
+        CC1101_Send(&CC1101_S);
+        osDelay(250);
+    }
+    /* USER CODE END StartTask_CC1101_S */
+}
+
+/**
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM6 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+    /* USER CODE BEGIN Callback 0 */
+
+    /* USER CODE END Callback 0 */
+    if(htim->Instance == TIM6) {
+        HAL_IncTick();
+    }
+    /* USER CODE BEGIN Callback 1 */
+
+    /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while(1) {
+    }
+    /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ cmake-build-debug/CMakeFiles/STM32_4.elf.dir/DependInfo.cmake
@@ -0,0 +1,100 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/XJBS/C/STM32_4/startup/startup_stm32f407xx.s" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/startup/startup_stm32f407xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/CC1101/Inc"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/XJBS/C/STM32_4/Core/Src/freertos.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Core/Src/freertos.c.obj"
+  "D:/XJBS/C/STM32_4/Core/Src/main.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Core/Src/main.c.obj"
+  "D:/XJBS/C/STM32_4/Core/Src/retarget.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Core/Src/retarget.c.obj"
+  "D:/XJBS/C/STM32_4/Core/Src/stm32f4xx_hal_msp.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "D:/XJBS/C/STM32_4/Core/Src/stm32f4xx_hal_timebase_tim.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.obj"
+  "D:/XJBS/C/STM32_4/Core/Src/stm32f4xx_it.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "D:/XJBS/C/STM32_4/Core/Src/syscalls.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Core/Src/syscalls.c.obj"
+  "D:/XJBS/C/STM32_4/Core/Src/system_stm32f4xx.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/CC1101/Src/CC1101.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/CC1101/Src/CC1101.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "D:/XJBS/C/STM32_4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/list.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj"
+  "D:/XJBS/C/STM32_4/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "D:/XJBS/C/STM32_4/cmake-build-debug/CMakeFiles/STM32_4.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/CC1101/Inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
